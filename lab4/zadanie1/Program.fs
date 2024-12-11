673ea388e99651a433f80b4523b5926a5dc74564
1Xarrr <118291472+1Xarrr@users.noreply.github.com>
2024-12-11 09:05:06 +0100

    Add files via upload

---
+++ lab4/zadanie1/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+
+// Definicja rekordu dla danych użytkownika
+type User = { Weight: float; Height: float }
+
+let calculateBMI weight height =
+    weight / ((height / 100.0) ** 2.0)
+
+let classifyBMI bmi =
+    if bmi < 18.5 then "Niedowaga"
+    elif bmi < 24.9 then "Prawidłowa waga"
+    elif bmi < 29.9 then "Nadwaga"
+    else "Otyłość"
+
+[<EntryPoint>]
+let main _ =
+    printfn "Podaj swoją wagę (kg):"
+    let weight = Console.ReadLine() |> float
+
+    printfn "Podaj swój wzrost (cm):"
+    let height = Console.ReadLine() |> float
+
+    let user = { Weight = weight; Height = height }
+    let bmi = calculateBMI user.Weight user.Height
+    let category = classifyBMI bmi
+
+    printfn "Twoje BMI: %.2f" bmi
+    printfn "Kategoria BMI: %s" category
+
+    0
