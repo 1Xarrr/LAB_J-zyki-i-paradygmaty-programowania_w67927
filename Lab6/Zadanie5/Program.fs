a077c88621bcee2ea65f25398f5fda750ac06f2c
1Xarrr <118291472+1Xarrr@users.noreply.github.com>
2025-01-21 22:11:07 +0100

    Add files via upload

---
+++ Lab6/Zadanie5/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let znajdzNajdluzszeSlowo (tekst: string) =
+    let slowa = tekst.Split([|' '; '\t'; '\n'; '\r'; '.'; ','; ';'; ':'; '!'; '?'|], StringSplitOptions.RemoveEmptyEntries)
+    if slowa.Length > 0 then
+        let najdluzsze = slowa |> Array.maxBy (fun s -> s.Length)
+        let dlugosc = najdluzsze.Length
+        najdluzsze, dlugosc
+    else
+        "", 0
+
+[<EntryPoint>]
+let main argv =
+    printfn "Wprowadź tekst:"
+    let tekst = Console.ReadLine()
+    let najdluzszeSlowo, dlugosc = znajdzNajdluzszeSlowo tekst
+    if dlugosc > 0 then
+        printfn "Najdłuższe słowo: \"%s\" (długość: %d)" najdluzszeSlowo dlugosc
+    else
+        printfn "Nie znaleziono żadnych słów w podanym tekście."
+    0
---
+++ Lab6/Zadanie6/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let zamienSlowo (tekst: string) (slowoDoZamiany: string) (noweSlowo: string) =
+    tekst.Replace(slowoDoZamiany, noweSlowo)
+
+[<EntryPoint>]
+let main argv =
+    printfn "Wprowadź tekst:"
+    let tekst = Console.ReadLine()
+    
+    printfn "Wprowadź słowo, które chcesz zamienić:"
+    let slowoDoZamiany = Console.ReadLine()
+    
+    printfn "Wprowadź nowe słowo:"
+    let noweSlowo = Console.ReadLine()
+    
+    let zmodyfikowanyTekst = zamienSlowo tekst slowoDoZamiany noweSlowo
+    
+    printfn "\nZmodyfikowany tekst:"
+    printfn "%s" zmodyfikowanyTekst
+    0
