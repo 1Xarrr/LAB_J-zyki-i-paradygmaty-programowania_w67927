9103a5ff6f4115b59fc277566f560d24ed4b7f55
1Xarrr <118291472+1Xarrr@users.noreply.github.com>
2025-01-21 22:08:11 +0100

    Add files via upload

---
+++ Laboratorium5/Zadanie3/Program.fs
@@ -0,0 +1,10 @@
+﻿let rec permutations list =
+    match list with
+    | [] -> [[]]
+    | _ ->
+        list |> List.collect (fun x ->
+            permutations (List.filter ((<>) x) list)
+            |> List.map (fun perm -> x :: perm))
+
+printfn "Permutacje listy [1;2;3]:"
+permutations [1;2;3] |> List.iter (printfn "%A")
