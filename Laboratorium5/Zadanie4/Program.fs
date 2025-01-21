3c83df433116d5639852c3adf939036c2514b2da
1Xarrr <118291472+1Xarrr@users.noreply.github.com>
2025-01-21 22:03:51 +0100

    Add files via upload

---
+++ Laboratorium5/Zadanie4/Program.fs
@@ -0,0 +1,18 @@
+﻿let rec hanoi n source auxiliary target =
+    if n > 0 then
+        hanoi (n - 1) source target auxiliary
+        printfn "Przenieś dysk %d z %s do %s" n source target
+        hanoi (n - 1) auxiliary source target
+
+let hanoiIterative n =
+    let moves = System.Collections.Generic.Stack<(int * string * string * string)>()
+    moves.Push(n, "A", "B", "C")
+    while moves.Count > 0 do
+        let (disks, src, aux, dst) = moves.Pop()
+        if disks > 0 then
+            moves.Push(disks - 1, aux, src, dst)
+            printfn "Przenieś dysk %d z %s do %s" disks src dst
+            moves.Push(disks - 1, src, dst, aux)
+
+printfn "Wieże Hanoi dla 3 dysków:"
+hanoi 3 "A" "B" "C"
---
+++ Laboratorium5/Zadanie5/Program.fs
@@ -0,0 +1,25 @@
+﻿let rec quickSort = function
+    | [] -> []
+    | pivot::rest ->
+        let smaller, greater = List.partition ((>=) pivot) rest
+        quickSort smaller @ [pivot] @ quickSort greater
+
+let quickSortIterative list =
+    let stack = System.Collections.Generic.Stack<List<int>>()
+    let mutable sorted = []
+    stack.Push(list)
+    while stack.Count > 0 do
+        match stack.Pop() with
+        | [] -> ()
+        | pivot::rest ->
+            let smaller, greater = List.partition ((>=) pivot) rest
+            stack.Push(greater)
+            sorted <- pivot :: sorted
+            stack.Push(smaller)
+    List.rev sorted
+
+printfn "Sortowanie QuickSort rekurencyjne:"
+printfn "%A" (quickSort [3;1;4;1;5;9;2;6;5;3;5])
+
+printfn "Sortowanie QuickSort iteracyjne:"
+printfn "%A" (quickSortIterative [3;1;4;1;5;9;2;6;5;3;5])
